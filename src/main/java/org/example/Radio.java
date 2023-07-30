69204dadd63de6aa6ca537bbe0fbd63fa1a05c60
Константин <belkovkostya.qa@mail.ru>
2023-07-30 21:22:55 +0300

    second commit

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -4,30 +4,29 @@ public class Radio {
     private int currentRadioStationNumber;
     private int soundVolume;
 
-    //constructor
-    public Radio (int currentRadioStationNumber, int soundVolume){
-        this.setCurrentRadioStationNumber(currentRadioStationNumber);
-        this.setSoundVolume(soundVolume);
-    }
+    /*  //constructor
+      public Radio (int currentRadioStationNumber, int soundVolume){
+          this.setCurrentRadioStationNumber(currentRadioStationNumber);
+          this.setSoundVolume(soundVolume);
+      }
 
-    //constructor
-    public Radio (){
-        this.setCurrentRadioStationNumber(0);
-        this.setSoundVolume(0);
-    }
-
+      //constructor
+      public Radio (){
+          this.setCurrentRadioStationNumber(0);
+          this.setSoundVolume(0);
+      }
+  */
     //getter
-    public int getCurrentRadioStationNumber(){
+    public int getCurrentRadioStationNumber() {
         return this.currentRadioStationNumber;
     }
 
     //setter
-    public void setCurrentRadioStationNumber(int currentRadioStationNumber){
-        if (currentRadioStationNumber < 0){
+    public void setCurrentRadioStationNumber(int currentRadioStationNumber) {
+        if (currentRadioStationNumber < 0) {
             this.currentRadioStationNumber = 9;
             return;
-        }
-        else if (currentRadioStationNumber > 9){
+        } else if (currentRadioStationNumber > 9) {
             this.currentRadioStationNumber = 0;
             return;
         }
@@ -35,46 +34,47 @@ public class Radio {
     }
 
     //getter
-    public int getSoundVolume(){
+    public int getSoundVolume() {
         return this.soundVolume;
     }
 
     //setter
-    public void setSoundVolume(int soundVolume){
-        if (soundVolume < 0){
+    public void setSoundVolume(int soundVolume) {
+        if (soundVolume < 0) {
             this.soundVolume = 0;
             return;
-        }
-        else if (soundVolume > 100){
+        } else if (soundVolume > 100) {
             this.soundVolume = 100;
             return;
         }
         this.soundVolume = soundVolume;
     }
 
-    public void prev(){
+    public void prev() {
         int nextRadioStationNumber = this.currentRadioStationNumber - 1;
         this.setCurrentRadioStationNumber(nextRadioStationNumber);
     }
 
-    public void next(){
+    public void next() {
         int nextRadioStationNumber = this.currentRadioStationNumber + 1;
         this.setCurrentRadioStationNumber(nextRadioStationNumber);
     }
 
-    public void volumeDecrease(){
+    public void volumeDecrease() {
         int soundVolume = this.soundVolume - 1;
         this.setSoundVolume(soundVolume);
     }
 
-    public void volumeIncrease(){
+    public void volumeIncrease() {
         int soundVolume = this.soundVolume + 1;
         this.setSoundVolume(soundVolume);
     }
 
-    public String showInfo(){
+   /* public String showInfo(){
         return
                 "Номер радиостанции: " + this.currentRadioStationNumber +
                         "\nГромкость: " + this.soundVolume;
     }
+
+    */
 }
